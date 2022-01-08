6bba511a1baf5bc038118a2ddd7304894b1d85b4
agrawaltejas01 <agrawaltejas01@gmail.com>
2022-01-08 18:58:05 +0530

    db operations carved out

--- app/repositories/users.go
+++ app/repositories/users.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"context"
-	"errors"
 	"time"
 
 	"github.com/agrawaltejas01/Go-MongoDB-BoilerPlate/db"
@@ -10,7 +9,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var usersModel *mongo.Collection
@@ -19,20 +17,15 @@ func init() {
 	usersModel, _ = db.GetCollection("users")
 }
 
-func findOne(collection *mongo.Collection, query bson.M, projection bson.M) (db.User, error) {
-	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	opts := options.FindOne().SetProjection(projection)
+func findOne(query bson.M, projection bson.M) (db.User, error) {
+	result := db.FindOne(usersModel, query, projection)
 
 	var user db.User
-	err := collection.FindOne(context, query, opts).Decode(&user)
-
+	err := result.Decode(&user)
 	return user, err
 }
 
 func buildUsersSlice(cursor *mongo.Cursor, context context.Context) ([]db.User, error) {
-	defer cursor.Close(context)
 	var result []db.User
 
 	for cursor.Next(context) {
@@ -45,20 +38,16 @@ func buildUsersSlice(cursor *mongo.Cursor, context context.Context) ([]db.User, 
 	if err := cursor.Err(); err != nil {
 		return nil, err
 	}
+	defer cursor.Close(context)
 	return result, nil
 }
 
-func find(collection *mongo.Collection, query bson.M, projection bson.M) ([]db.User, error) {
-	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	opts := options.Find().SetProjection(projection)
-
-	cur, err := collection.Find(context, query, opts)
+func find(query bson.M, projection bson.M) ([]db.User, error) {
+	cursor, context, err := db.Find(usersModel, query, projection)
 	if err != nil {
 		return nil, err
 	}
-	return buildUsersSlice(cur, context)
+	return buildUsersSlice(cursor, context)
 }
 
 func insertOne(collection *mongo.Collection, user db.User) (primitive.ObjectID, error) {
@@ -70,63 +59,27 @@ func insertOne(collection *mongo.Collection, user db.User) (primitive.ObjectID, 
 	return result.InsertedID.(primitive.ObjectID), err
 }
 
-func updateOne(collection *mongo.Collection, filter bson.M, update bson.M) error {
-	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	result, err := collection.UpdateOne(context, filter, update)
-
-	if result.MatchedCount == 0 {
-		panic(errors.New("no docs matched"))
-	} else if result.ModifiedCount == 0 {
-		panic(errors.New("no docs modified"))
-	}
-
-	if err != nil {
-		panic(err)
-	}
-
-	return nil
-}
-
-func deleteOne(collection *mongo.Collection, filter bson.M) error {
-	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	result, err := collection.DeleteOne(context, filter)
-
-	if result.DeletedCount == 0 {
-		panic(errors.New("no docs deleted"))
-	}
-
-	if err != nil {
-		panic(err)
-	}
-
-	return nil
-}
-
 func GetUsers() ([]db.User, error) {
-	return find(usersModel, bson.M{}, bson.M{})
+	return find(bson.M{}, bson.M{})
 }
 
 func GetUser(id primitive.ObjectID) (db.User, error) {
-	return findOne(usersModel, bson.M{"_id": id}, bson.M{})
+	return findOne(bson.M{"_id": id}, bson.M{})
 }
 
 func AddUser(user db.User) (primitive.ObjectID, error) {
 	return insertOne(usersModel, user)
 }
 
-func UpdateUser(id primitive.ObjectID) error {
+func UpdateUser(_id primitive.ObjectID) error {
 	update := bson.M{
 		"$set": bson.M{
-			"name": "Tejas",
+			"name": "TSA",
 		},
 	}
-	return updateOne(usersModel, bson.M{"_id": id}, update)
+	return db.UpdateOne(usersModel, bson.M{"_id": _id}, update)
 }
 
-func DeleteUser(id primitive.ObjectID) error {
-	return deleteOne(usersModel, bson.M{"_id": id})
+func DeleteUser(_id primitive.ObjectID) error {
+	return db.DeleteOne(usersModel, bson.M{"_id": _id})
 }
---
+++ db/operations.go
@@ -0,0 +1,107 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func FindOne(collection *mongo.Collection, query bson.M, projection bson.M) *mongo.SingleResult {
+	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	opts := options.FindOne().SetProjection(projection)
+
+	result := collection.FindOne(context, query, opts)
+
+	return result
+}
+
+func Find(collection *mongo.Collection, query bson.M, projection bson.M) (*mongo.Cursor, context.Context, error) {
+	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	opts := options.Find().SetProjection(projection)
+
+	cur, err := collection.Find(context, query, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+	return cur, context, nil
+}
+
+func UpdateOne(collection *mongo.Collection, filter bson.M, update bson.M) error {
+	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := collection.UpdateOne(context, filter, update)
+
+	if result.MatchedCount == 0 {
+		panic(errors.New("no docs matched"))
+	} else if result.ModifiedCount == 0 {
+		panic(errors.New("no docs modified"))
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func UpdateMany(collection *mongo.Collection, filter bson.M, update bson.M) error {
+	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := collection.UpdateMany(context, filter, update)
+
+	if result.MatchedCount == 0 {
+		panic(errors.New("no docs matched"))
+	} else if result.ModifiedCount == 0 {
+		panic(errors.New("no docs modified"))
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func DeleteOne(collection *mongo.Collection, filter bson.M) error {
+	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := collection.DeleteOne(context, filter)
+
+	if result.DeletedCount == 0 {
+		panic(errors.New("no docs deleted"))
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func DeleteMany(collection *mongo.Collection, filter bson.M) error {
+	context, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := collection.DeleteMany(context, filter)
+
+	if result.DeletedCount == 0 {
+		panic(errors.New("no docs deleted"))
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
